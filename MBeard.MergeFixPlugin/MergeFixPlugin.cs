cfeb2d399e3fb95d33e6022651bea3709d58aa19
Matt Beard <mattybeard@gmail.com>
2020-08-14 18:43:04 +0100

    Init Commit

---
+++ MBeard.MergeFixPlugin/MergeFixPlugin.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using System;
+
+
+namespace MBeard
+{
+    public class MergeFixPlugin : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            if (context.MessageName != "Merge")
+                return;
+
+            var mergeReq = new MergeRequest()
+            {
+                Parameters = context.InputParameters
+            };
+            mergeReq.PerformParentingChecks = false;
+        }
+    }
+}
